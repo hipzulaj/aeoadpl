fb3a599e583b81623f3e860e3b4836b1b35a072b
najwa1996 <32484190+najwa1996@users.noreply.github.com>
2017-10-04 17:45:13 +0700

    edit produk ama hapus produk

--- application/controllers/eo.php
+++ application/controllers/eo.php
@@ -56,37 +56,7 @@ class eo extends CI_Controller {
 		}
 	}
 
-	public function Edit_produk($id){
+	
 
-		$this->form_validation->set_rules('nama', 'Nama Produk', 'required');
-		$this->form_validation->set_rules('harga', 'Harga', 'required');
-		$this->form_validation->set_rules('deskripsi', 'Deskripsi', 'required');
-		$this->form_validation->set_rules('jenis', 'Jenis Produk', 'required');
-
-		if($this->form_validation->run() == false){
-			$data['produk'] = $this->Model_products->find($id);
-			$this->load->view('eo/Edit_produk_form', $data);
-
-			if(isset($this->session->userdata['eo'])){
-    			$this->load->view('eo/Edit_produk_form');
-			}
-			else
-				redirect('display/login');
-		}
-		else {
-			$data_products = array(
-				'nama_produk' => set_value('nama'),
-				'biaya' => set_value('harga'),
-				'deskripsi' => set_value('deskripsi'),
-				'jenis' => set_value('jenis'), 
-				);
-			$this->Model_products->Edit_products($id, $data_products);
-			redirect('display/Dashboard_eo');
-		}
-	}
-
-	public function Hapus_produk($id){
-		$this->Model_products->Delete_products($id);
-		redirect('display/Dashboard_eo');
-	}
+	
 }
