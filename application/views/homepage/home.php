740b2e130e116a5831b1f2f46a0ccff2c2f94301
almirafilia <almira.filiaz@gmail.com>
2017-10-04 17:40:35 +0700

    Nyoba Ganti

--- application/views/homepage/home.php
+++ application/views/homepage/home.php
@@ -115,7 +115,7 @@
           <?php foreach ($products as $product) : ?>
             <div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <a href="#"><img class="card-img-top" src="http://images.locanto.ae/1646915967/Birthday-Planner-and-Event-Organizer-in-Dubai_1.jpg" alt=""></a>
                 <div class="card-body">
                   <h4 class="card-title">
                     <a href="#"><?=$product->nama_produk?></a>
